fc40af5d1a42ac5d609b291e4e20958d2b8f4b3c
f1nesser <riiw0046@colorado.edu>
2022-05-08 11:49:43 -0600

    clean

--- pages/images/[image].js
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image'
-import Router, { useRouter } from 'next/router'
-export default function Images() {
-	const router = useRouter()
-	const { image } = router.query
-	const path = `/${image}`
-	return <Image src={path} alt='' layout='fill' />
-}
