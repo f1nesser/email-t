71128d47cbd28a15f6886b38fb4b25e81a96d803
f1nesser <riiw0046@colorado.edu>
2022-05-16 18:07:08 -0600

    switch

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const logger = (req, res, next) => {
-	console.log(req.url)
-	console.dir(req.ip)
-	let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-	console.log(ip)
+	if (req.method === 'GET') {
+		let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+		console.log(`${ip} requested ${req.url}`)
+	}
 	next()
 }
 
@@ -18,8 +18,10 @@ app.get('/', (req, res) => {
 	res.send('hello world')
 })
 
-app.get(/h/, (req, res) => {
-	console.log(req)
+app.get('/create', (req, res) => {
+	let { name } = req.query
+
+	res.send(name)
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
