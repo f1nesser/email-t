1ba640a09a65c53e131e1d95198c98f737543bbc
f1nesser <riiw0046@colorado.edu>
2022-05-29 22:31:49 -1000

    everything seems to work

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
-const express = require('express')
+import express from 'express'
+import fs from 'fs'
+import prismaPkg from '@prisma/client'
+const { PrismaClient } = prismaPkg
+const prisma = new PrismaClient()
 const app = express()
 const port = process.env.PORT || 3000
-const logger = (req, res, next) => {
-    if (req.method === 'GET') {
-        let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-        console.log(`${ip} requested ${req.url}`)
+
+const getCurDate = () => new Date(Date.now()).toISOString()
+const emailExists = async (id) => {
+    return prisma.email.findUnique({
+        where: {
+            id: id,
+        },
+    })
+}
+
+const logger = async (req, res, next) => {
+    const isGet = req.method === 'GET'
+    const correctFormat = req.originalUrl.match(/\/[0-9]{1,}.png/)
+
+    if (!(isGet && correctFormat)) {
+        console.log('logger not applicable')
+        next()
+        return
+    }
+
+    let emailFile = req.url.split('/')[1]
+
+    let data = {
+        /*
+        shouldve done prisma migration to create the db
+        this would've avoided this non-camelCase naming
+        heroku did not allow this however 
+        */
+        ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+        emailid: parseInt(emailFile.split('.png')[0]),
+        viewdate: getCurDate(),
+    }
+
+    let fileExists = fs.existsSync(`./images/${emailFile}`)
+    let emailInDb = await emailExists(data.emailid)
+
+    if (fileExists && emailInDb) {
+        let viewAdded = await prisma.view.create({ data })
+        console.log({ message: viewAdded })
+    } else {
+        res.json({ message: `not tracking ${emailFile}` })
     }
     next()
 }
@@ -12,15 +53,50 @@ const logger = (req, res, next) => {
 app.use(logger)
 app.use(express.static('images'))
 
-app.get('/', (req, res) => {
-    // console.log(req)
-    res.send('hello world')
+app.get('/status/:id', async (req, res) => {
+    let id = parseInt(req.params.id)
+    let views = await prisma.view.findMany({
+        where: {
+            emailid: id,
+        },
+    })
+    res.json({ message: views })
 })
 
-app.get('/create', (req, res) => {
-    let { name } = req.query
+app.get('/create', async (req, res) => {
+    const MAX = 10000
+    const MIN = 1
+    const { subject } = req.query
+    const rightNow = getCurDate()
+    let id,
+        repeat = 1
+    if (!subject) {
+        res.json({ message: 'no subject' })
+        return
+    }
+    while (repeat) {
+        id = Math.floor(Math.random() * MAX) + MIN
+        repeat = await prisma.email.findUnique({
+            where: {
+                id: id,
+            },
+        })
+    }
 
-    res.send(name)
+    fs.copyFileSync('./baseImage/invisible.png', `./images/${id}.png`)
+
+    const email = await prisma.email.create({
+        data: {
+            id: id,
+            createdat: rightNow,
+            subject: subject,
+        },
+    })
+    if (email) {
+        res.json({ id: id, subject: subject })
+    } else {
+        res.json({ message: 'failed to add email' })
+    }
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
